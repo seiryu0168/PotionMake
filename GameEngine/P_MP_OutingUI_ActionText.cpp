59870aa53ed00ff15b438b7f39e16312cf62f4a0
成龍 長尾 <210215@jc-21.jp>
2024-03-24 21:16:29 +0900

    外出用のセレクトUIの画像追加

--- GameEngine/P_MP_OutingUI_ActionText.cpp
+++ GameEngine/P_MP_OutingUI_ActionText.cpp
@@ -12,6 +12,11 @@ P_MP_OutingUI_ActionText::~P_MP_OutingUI_ActionText()
 
 void P_MP_OutingUI_ActionText::Initialize()
 {
+	Image selectImage(this);
+	selectImage.Load("Assets/Image/SelectImage1.png");
+	selectImage.SetSize({ 1,0.2f,0 });
+	selectImage.SetPosition({ 0,-0.02f,0 });
+	AddComponent<Image>(selectImage);
 	Text actionText(this);
 	actionText.SetAlignmentType(ALIGNMENT_TYPE::CENTER_TOP);
 	actionText.SetText("çÃèWÇ…èoÇ©ÇØÇÈ\nîÑÇËÇ…çsÇ≠");
@@ -25,6 +30,7 @@ void P_MP_OutingUI_ActionText::Start()
 
 void P_MP_OutingUI_ActionText::Update()
 {
+
 }
 
 void P_MP_OutingUI_ActionText::Release()
