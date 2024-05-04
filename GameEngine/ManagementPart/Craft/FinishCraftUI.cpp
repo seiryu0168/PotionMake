f45143dbe2042ea8665d139ed92e58aecf642238
成龍 長尾 <210215@jc-21.jp>
2024-05-04 16:40:52 +0900

    ポーション制作後のリザルト表示を変更

--- GameEngine/ManagementPart/Craft/FinishCraftUI.cpp
+++ GameEngine/ManagementPart/Craft/FinishCraftUI.cpp
@@ -8,7 +8,7 @@
 FinishCraftUI::FinishCraftUI(Object* parent)
 	:GameObject(parent,"FinishCraftUI"),
 	okButtonImageNum_(-1),
-	uiPos_({ 0,-0.6f,0 })
+	uiPos_({ 0,0,0 })
 {
 }
 
@@ -18,12 +18,13 @@ FinishCraftUI::~FinishCraftUI()
 
 void FinishCraftUI::Initialize()
 {
-	//背景の画像
-	Image backImage(this);
-	backImage.Load("Assets/Image/PotionManagerUIBase1.png");
-	backImage.SetSize({ 2,2,0 });
-	backImage.SetLayer(2);
-	AddComponent<Image>(backImage);
+	CreateBase();
+	////背景の画像
+	//Image backImage(this);
+	//backImage.Load("Assets/Image/PotionManagerUIBase1.png");
+	//backImage.SetSize({ 2,2,0 });
+	//backImage.SetLayer(2);
+	//AddComponent<Image>(backImage);
 	
 	//ポーションのデータを取得
 	PlayerData* data = InterSceneData::GetData<PlayerData>("Data01");
@@ -32,6 +33,7 @@ void FinishCraftUI::Initialize()
 	//ポーションのボトル部分の画像
 	Image potionImage(this);
 	potionImage.Load("Assets/Image/Potion_BaseImage.png");
+	potionImage.SetPosition({ 0, 0.3f, 0 });
 	potionImage.SetLayer(2);
 	potionImage.SetColor(pData.potionColor_);
 	AddComponent<Image>(potionImage);
@@ -39,10 +41,10 @@ void FinishCraftUI::Initialize()
 	//ポーションの縁の画像
 	Image edgeImage(this);
 	edgeImage.Load("Assets/Image/Potion_EdgeImage.png");
+	edgeImage.SetPosition({ 0,0.3f,0 });
 	edgeImage.SetLayer(2);
 	AddComponent<Image>(edgeImage);
 
-	CreateBase();
 	////OKボタンの画像
 	//Image okButtonImage(this);
 	//okButtonImage.Load("Assets/Image/ItemBaseImage.png");
@@ -61,11 +63,16 @@ void FinishCraftUI::Initialize()
 	potionNameText.SetPosition({ 500,600 });
 	AddComponent<Text>(potionNameText);
 
+	Image OKImage(this);
+	OKImage.Load("Assets/Image/ButtonImage02.png");
+	OKImage.SetPosition({ 0,-0.6f,0 });
+	OKImage.SetLayer(2);
+	okButtonImageNum_ = AddComponent<Image>(OKImage);
 
 	Text OKText(this);
 	OKText.SetText("OK");
 	OKText.SetLayer(2);
-	OKText.SetPosition({ 900,810 });
+	OKText.SetPosition({ 900,840 });
 	AddComponent<Text>(OKText);
 
 	hAudio_Success_ = Audio::Load("Assets/Audio/Success01.wav");
@@ -89,54 +96,55 @@ void FinishCraftUI::CreateBase()
 {
 	XMFLOAT3 windowSize;
 	XMFLOAT3 imageRatio;
+	float edgeSize = 1.0f;
 	Image uiBaseImage(this);
 	uiBaseImage.Load("Assets/Image/UIBaseImage2.png");
 	uiBaseImage.SetPosition({ uiPos_.x,uiPos_.y,0 });
-	uiBaseImage.SetSize({ 2 * 2,2 * 1,0 });
+	uiBaseImage.SetSize({ 16.0f*(Direct3D::GetScreenWidth() /1024.0f), 16.0f*(Direct3D::GetScreenHeight()/ 1024.0f), 0});
 	uiBaseImage.SetLayer(2);
 	windowSize = uiBaseImage.GetSizeAtPixel();
 	imageRatio = uiBaseImage.GetSize();
-	okButtonImageNum_ = AddComponent<Image>(uiBaseImage);
+	AddComponent<Image>(uiBaseImage);
 
 	windowSize = { windowSize.x / (Direct3D::GetScreenWidth()),
 				   windowSize.y / (Direct3D::GetScreenHeight()),0 };
 	Image uiWidthEdgeImage1(this);
 	uiWidthEdgeImage1.Load("Assets/Image/UIEdge_Width.png");
 	uiWidthEdgeImage1.SetPosition({ uiPos_.x,uiPos_.y + windowSize.y * 0.9f,0 });
-	uiWidthEdgeImage1.SetSize({ imageRatio.x * 0.95f,0.3f,0 });
+	uiWidthEdgeImage1.SetSize({ imageRatio.x * 0.95f,edgeSize,0 });
 	uiWidthEdgeImage1.SetLayer(2);
 	AddComponent<Image>(uiWidthEdgeImage1);
 	Image uiWidthEdgeImage2(this);
 	uiWidthEdgeImage2.Load("Assets/Image/UIEdge_Width.png");
 	uiWidthEdgeImage2.SetPosition({ uiPos_.x,uiPos_.y - windowSize.y * 0.9f,0 });
-	uiWidthEdgeImage2.SetSize({ imageRatio.x * 0.95f,0.3f,0 });
+	uiWidthEdgeImage2.SetSize({ imageRatio.x * 0.95f,edgeSize,0 });
 	uiWidthEdgeImage2.SetLayer(2);
 	AddComponent<Image>(uiWidthEdgeImage2);
 
 	Image uiHeightEdgeImage1(this);
 	uiHeightEdgeImage1.Load("Assets/Image/UIEdge_Height.png");
 	uiHeightEdgeImage1.SetPosition({ uiPos_.x + windowSize.x * 0.95f,uiPos_.y,0 });
-	uiHeightEdgeImage1.SetSize({ 0.3f,imageRatio.y * 0.9f,0 });
+	uiHeightEdgeImage1.SetSize({ edgeSize,imageRatio.y * 0.9f,0 });
 	uiHeightEdgeImage1.SetLayer(2);
 	AddComponent<Image>(uiHeightEdgeImage1);
 	Image uiHeightEdgeImage2(this);
 	uiHeightEdgeImage2.Load("Assets/Image/UIEdge_Height.png");
 	uiHeightEdgeImage2.SetPosition({ uiPos_.x - windowSize.x * 0.95f,uiPos_.y,0 });
-	uiHeightEdgeImage2.SetSize({ 0.3f,imageRatio.y * 0.9f,0 });
+	uiHeightEdgeImage2.SetSize({ edgeSize,imageRatio.y * 0.9f,0 });
 	uiHeightEdgeImage2.SetLayer(2);
 	AddComponent<Image>(uiHeightEdgeImage2);
 
 	Image uiCornerImage1(this);
 	uiCornerImage1.Load("Assets/Image/UIEdge_Corner.png");
 	uiCornerImage1.SetPosition({ uiPos_.x + windowSize.x * 0.95f,uiPos_.y + windowSize.y * 0.901f,0 });
-	uiCornerImage1.SetSize({ 0.3f,0.3f,0 });
+	uiCornerImage1.SetSize({ edgeSize,edgeSize,0 });
 	uiCornerImage1.SetLayer(2);
 	AddComponent<Image>(uiCornerImage1);
 
 	Image uiCornerImage2(this);
 	uiCornerImage2.Load("Assets/Image/UIEdge_Corner.png");
 	uiCornerImage2.SetPosition({ uiPos_.x + windowSize.x * 0.9495f,uiPos_.y - windowSize.y * 0.901f,0 });
-	uiCornerImage2.SetSize({ 0.3f,0.3f,0 });
+	uiCornerImage2.SetSize({ edgeSize,edgeSize,0 });
 	uiCornerImage2.SetLayer(2);
 	uiCornerImage2.SetRotation({ 0,0,-90 });
 	AddComponent<Image>(uiCornerImage2);
@@ -144,7 +152,7 @@ void FinishCraftUI::CreateBase()
 	Image uiCornerImage3(this);
 	uiCornerImage3.Load("Assets/Image/UIEdge_Corner.png");
 	uiCornerImage3.SetPosition({ uiPos_.x - windowSize.x * 0.9505f,uiPos_.y - windowSize.y * 0.901f,0 });
-	uiCornerImage3.SetSize({ 0.3f,0.3f,0 });
+	uiCornerImage3.SetSize({ edgeSize,edgeSize,0 });
 	uiCornerImage3.SetLayer(2);
 	uiCornerImage3.SetRotation({ 0,0,180 });
 	AddComponent<Image>(uiCornerImage3);
@@ -152,7 +160,7 @@ void FinishCraftUI::CreateBase()
 	Image uiCornerImage4(this);
 	uiCornerImage4.Load("Assets/Image/UIEdge_Corner.png");
 	uiCornerImage4.SetPosition({ uiPos_.x - windowSize.x * 0.95f,uiPos_.y + windowSize.y * 0.9f,0 });
-	uiCornerImage4.SetSize({ 0.3f,0.3f,0 });
+	uiCornerImage4.SetSize({ edgeSize,edgeSize,0 });
 	uiCornerImage4.SetLayer(2);
 	uiCornerImage4.SetRotation({ 0,0,90 });
 	AddComponent<Image>(uiCornerImage4);
