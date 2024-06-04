b8afe04df10f4315effeb3070ffeac4910a610b9
成龍 長尾 <210215@jc-21.jp>
2024-06-04 15:38:00 +0900

    ステータスのフォント変えた

--- GameEngine/Status.cpp
+++ GameEngine/Status.cpp
@@ -4,6 +4,10 @@
 #include"Engine/DirectX_11/Input.h"
 #include"InterSceneData.h"
 #include"PlayerData.h"
+namespace
+{
+	float TextSize = 60.0f;
+}
 Status::Status(Object* parent)
 	:UIBase(parent,"Status")
 {
@@ -24,23 +28,27 @@ void Status::Initialize()
 
 	XMFLOAT3 pos = confirmation.GetPositionAtPixel();
 	XMFLOAT3 size = confirmation.GetSizeAtPixel();
-	Text name(this);
+	Text name(this,"Rounded M+ 1c");
 	name.SetText("ñºëO:"+pData.name_);
+	name.SetTextSize(TextSize);
 	name.SetPosition({ pos.x - size.x * 0.5f + 30,pos.y - size.y*0.4f });
 	nameTextNum_ = AddComponent<Text>(name);
 
-	Text  money(this);
+	Text  money(this, "Rounded M+ 1c");
 	money.SetText("èäéùã‡:" + std::to_string(pData.money_));
+	money.SetTextSize(TextSize);
 	money.SetPosition({ pos.x - size.x * 0.5f + 30,pos.y - size.y * 0.4f + 100 });
 	moneyTextNum_ = AddComponent<Text>(money);
 	
-	Text  craftCount(this);
+	Text  craftCount(this, "Rounded M+ 1c");
 	craftCount.SetText("êßçÏâÒêî:"+std::to_string(pData.craftCount_));
+	craftCount.SetTextSize(TextSize);
 	craftCount.SetPosition({ pos.x - size.x * 0.5f + 30,pos.y - size.y * 0.4f+200 });
 	moneyTextNum_ = AddComponent<Text>(craftCount);
 	
-	Text  sellCount(this);
+	Text  sellCount(this, "Rounded M+ 1c");
 	sellCount.SetText("îÃîÑâÒêî:" + std::to_string(pData.sellCount_));
+	sellCount.SetTextSize(TextSize);
 	sellCount.SetPosition({ pos.x - size.x * 0.5f + 30,pos.y - size.y * 0.4f + 300 });
 	moneyTextNum_ = AddComponent<Text>(sellCount);
 
